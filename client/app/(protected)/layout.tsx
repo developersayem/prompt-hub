eb33c89ac07035e239c57bfed1b084a3a4e5623e
Sayem Molla <developersayem@outlook.com>
2025-06-30 01:18:29 +0600

    protect some routes

---
+++ client/app/(protected)/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useAuth } from "@/contexts/auth-context";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isLoading, isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      router.push("/auth/login");
+    }
+  }, [isLoading, isAuthenticated, router]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!isAuthenticated) return null;
+
+  return <>{children}</>;
+}
