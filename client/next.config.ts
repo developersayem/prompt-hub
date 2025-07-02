bab46536116ba465bd8106c3f5b2f6c9f9f285dd
Sayem Molla <developersayem@outlook.com>
2025-07-02 16:58:06 +0600

    update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["lh3.googleusercontent.com","res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
