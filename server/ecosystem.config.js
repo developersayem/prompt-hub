4b341bef3a335d01a46df6a99a8d2a18b09d2e13
Sayem Molla <developersayem@outlook.com>
2025-07-07 18:22:43 +0600

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "backend",
+      name: "prompt-hub-backend",
       script: "./dist/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
       env: {
         NODE_ENV: "production",
-        PORT: "5001", // or another allowed port
       },
     },
   ],
