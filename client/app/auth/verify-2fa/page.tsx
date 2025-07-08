03201b56fa4925e26af384e0f07914083682d418
Sayem Molla <developersayem@outlook.com>
2025-07-08 16:24:53 +0600

    fix: nextjs useSearchParams problem

--- client/app/auth/verify-2fa/page.tsx
+++ client/app/auth/verify-2fa/page.tsx
@@ -1,9 +1,19 @@
-import Verify2FACom from "./verify-2fa-com";
+"use client";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+// Optionally you can create a fallback loader component
+const Verify2FACom = dynamic(() => import("./verify-2fa-com"), {
+  ssr: false, // Disable SSR to avoid `useSearchParams` issues
+});
 
 export default function Verify2FAPage() {
   return (
     <div className="max-h-screen max-w-screen flex items-center justify-center">
-      <Verify2FACom />
+      <Suspense fallback={<p>Loading 2FA Verification...</p>}>
+        <Verify2FACom />
+      </Suspense>
     </div>
   );
 }
