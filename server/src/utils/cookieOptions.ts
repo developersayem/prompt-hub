580809cf5fb424bb1c7cb2bde2f0bcf00e770c4d
Sayem Molla <developersayem@outlook.com>
2025-07-09 21:44:40 +0600

    cookie option updated

--- server/src/utils/cookieOptions.ts
+++ server/src/utils/cookieOptions.ts
@@ -1,13 +1,14 @@
-// cookieOptions.ts
 import { CookieOptions } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 export const cookieOptions: CookieOptions = {
- httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  domain: "app.shopxet.com", // ✅ update this!
+  httpOnly: true,
+  secure: isProduction, // secure only in production
+  sameSite: isProduction ? "none" : "lax", // lax in local, none in prod
+  domain: isProduction ? ".shopxet.com" : undefined, // no domain for localhost
   path: "/",
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
