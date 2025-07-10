4f0bf5f993728354ddb3fc1c6d65db5c3a398181
Sayem Molla <developersayem@outlook.com>
2025-07-10 22:24:04 +0600

    update protected layout.tsx

--- client/app/(protected)/layout.tsx
+++ client/app/(protected)/layout.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { useAuth } from "@/contexts/auth-context";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isAuthenticated, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      router.replace("/auth/login");
+    }
+  }, [isLoading, isAuthenticated, router]);
+
+  if (isLoading) {
+    return <p className="text-center">Checking authentication...</p>;
+  }
+
+  if (!isAuthenticated) {
+    return null; // ⏳ wait while redirecting
+  }
+
   return <>{children}</>;
 }
